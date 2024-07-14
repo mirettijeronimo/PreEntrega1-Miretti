dcee125b90583f165462e91c488e2ad2d9c7e7e5
Jeronimo Miretti <jerono.miretti@gmail.com>
2024-07-14 17:45:11 -0300

    preparativos tercera entrega

---
+++ javascript/codigo.js
@@ -0,0 +1,8 @@
+// Acertijo sencillo, nos dice “¿Cómo podemos hacer que cuatro nueves den como resultado cien?”
+// 9/9+99=100
+
+
+let boton = document.getElementById("btnPrincipal");
+boton.addEventListener("click", function() {
+    
+});
--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,5 @@
-
+let puntos = 0;
+let vidas = 3;
 
 function introduccion() {
     alert("Hola, esta es una aventura de Mario donde resolverás ejercicios matemáticos para ayudarlo a rescatar a la princesa.");
@@ -21,6 +22,24 @@ function mostrarTres() {
     alert("¿Siguiente ejercicio?");
 }
 
+function drawScore() {
+    document.getElementById('puntosActuales').innerText = 'Puntos actuales: ' + puntos;
+}
+
+function drawVidas() {
+    document.getElementById('vidasRestantes').innerText = 'Vidas restantes: ' + vidas;
+}
+
+function reiniciarJuego() {
+    puntos = 0;
+    vidas = 3;
+    drawScore();
+    drawVidas();
+    document.getElementById('resultadoFinal').innerText = '';
+    alert("El juego se reiniciará ahora.");
+    iniciarJuego();
+}
+
 function elegirOpcionCorrecta(pregunta, respuestaCorrecta) {
     let volverAIntentarlo = true;
 
@@ -29,10 +48,20 @@ function elegirOpcionCorrecta(pregunta, respuestaCorrecta) {
         if (respuesta === respuestaCorrecta) {
             alert("¡Felicitaciones! " + respuesta + " es la opción correcta.");
             volverAIntentarlo = false;
+            puntos++;
         } else {
             alert("Respuesta incorrecta. Vuelve a intentarlo.");
+            vidas--;
+            drawVidas();
+            if (vidas <= 0) {
+                alert("Game Over! Se han agotado todas las vidas.");
+                reiniciarJuego();
+                return; // Salir de la función si se acaban las vidas
+            }
         }
     } while (volverAIntentarlo);
+
+    drawScore();
 }
 
 const resultados = [
@@ -63,14 +92,32 @@ function mostrarOpcionesDos() {
     elegirOpcionCorrecta("Ingrese la respuesta correcta (A, B, C, D)", "A");
 }
 
+function mostrarResultadosFinales() {
+    document.getElementById('resultadoFinal').innerText = 'Puntuación final: ' + puntos + ', Vidas restantes: ' + vidas;
+}
 
-let boton = document.getElementById("btnPrincipal");
-boton.addEventListener("click", function() {
+function iniciarJuego() {
     introduccion();
     solicitarNombre();
     alert("Vamos a resolver esta suma 5 + 5 = ?");
-    elegirOpcionCorrecta("Ingrese la respuesta correcta ", "10");
+    elegirOpcionCorrecta("Ingrese la respuesta correcta", "10");
     mostrar();
     mostrarDos();
     mostrarOpciones();
+    mostrarResultadosFinales();
+}
+
+let boton = document.getElementById("btnPrincipal");
+boton.addEventListener("click", function() {
+    iniciarJuego();
 });
+
+
+//tabla de puntos localstorage setItem / getItem 
+
+// sessionStorage.setItem("array", [1,2,3]);
+// let array = sessionStorage.getItem("array");
+// console.log(array)
+
+
+
